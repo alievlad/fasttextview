9f041f65e980f4bfb3d185092b53e98d9eec9a20
Vlad Aliev <aliev.vlad@payme.uz>
2022-04-13 18:32:36 +0500

    Initialization commit: removed unused resources, added FastTextView.kt

---
+++ app/src/main/java/com/alievlad/fasttextview/FastTextView.kt
@@ -0,0 +1,36 @@
+package com.alievlad.fasttextview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.text.PrecomputedTextCompat
+import androidx.core.widget.TextViewCompat
+import kotlinx.coroutines.*
+
+
+/**
+ * Created by alievlad on 4/13/2022.
+ */
+
+class FastTextView(
+	context: Context,
+	attrs: AttributeSet?
+) : AppCompatTextView(context, attrs) {
+
+	private var deferred : Deferred<Unit>? = null
+
+	override fun setText(text: CharSequence, type: BufferType?) {
+
+		deferred?.cancel()
+
+		deferred = CoroutineScope(Dispatchers.Main).async {
+
+			val computedText = withContext(Dispatchers.IO) {
+				val params = TextViewCompat.getTextMetricsParams(this@FastTextView)
+				PrecomputedTextCompat.create(text, params)
+			}
+
+			TextViewCompat.setPrecomputedText(this@FastTextView, computedText)
+		}
+	}
+}
